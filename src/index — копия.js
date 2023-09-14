3b72161e89a5f652be132a4bb5168efa93adce4c
777Afanas <serhii2111@yahoo.com>
2023-09-14 13:13:17 +0300

    markup plus

--- src/index — копия.js
+++ src/index — копия.js
@@ -1,10 +1,8 @@
-
 const refs = {
   searchForm: document.querySelector('.search-form'),
   photosContainer: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
-
 
 // const fetchPostsBtn = document.querySelector(".btn");
 // const postList = document.querySelector(".posts");
@@ -18,25 +16,7 @@ const refs = {
 // // In our case total number of pages is calculated on frontend
 // const totalPages = 100 / limit;
 
-// fetchPostsBtn.addEventListener("click", () => {
-//   // Check the end of the collection to display an alert
-//   if (page > totalPages) {
-//     return toggleAlertPopup();
-//   }
-
-//   fetchPosts()
-//     .then((posts) => {
-//       renderPosts(posts);
-//       // Increase the group number
-//       page += 1;
-
-//       // Replace button text after first request
-//       if (page > 1) {
-//         fetchPostsBtn.textContent = "Fetch more posts";
-//       }
-//     })
-//     .catch((error) => console.log(error));
-// });
+// refs.searchForm.addEventListener('submit', onSearch);
 
 refs.searchForm.addEventListener('submit', onSearch);
 
@@ -60,53 +40,64 @@ function onSearch(e) {
     .catch(error => console.log(error));
 }
 
-// function fetchPosts() {
-//   const params = new URLSearchParams({
-//     _limit: limit,
-//     _page: page
-//   });
+// function onSearch(e) {
+//     e.preventDefault();
 
-//   return fetch(`https://jsonplaceholder.typicode.com/posts?${params}`).then(
-//     (response) => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     }
-//   );
-// }
+//     // console.log(fetchPhotos());
 
- function fetchPhotos() {
-    
-        const BASE_URL = 'https://pixabay.com/api';
-        const API_KEY = '39342201-f813eddd1adb93dcbf05db88a';
-        const params = new URLSearchParams({
-            per_page: 3,
-            page: 1
-        });
-        
-        const url = `${BASE_URL}/?key=${API_KEY}&
+//     fetchPhotos()
+//         .then((data) => {
+//             console.log(data.hits);
+//       renderPosts();
+//     //   // Increase the group number
+//     //   page += 1;
+
+//     //   // Replace button text after first request
+//     //   if (page > 1) {
+//     //     fetchPostsBtn.textContent = "Fetch more posts";
+//     //   }
+//     })
+//     .catch((error) => console.log(error));
+
+function fetchPhotos() {
+  const BASE_URL = 'https://pixabay.com/api';
+  const API_KEY = '39342201-f813eddd1adb93dcbf05db88a';
+  const params = new URLSearchParams({
+    per_page: 3,
+    page: 1,
+  });
+
+  const url = `${BASE_URL}/?key=${API_KEY}&
     q=yellow+flower&image_type=photo&orientation=horizontal&
     safesearch=true&${params}`;
 
-        return fetch(url)
-            .then(
-                (response) => {
-                    if (!response.ok) {
-                        throw new Error(response.status);
-                    }
-                   
-                    return response.json();
-                    
-                });
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
     }
 
+    return response.json();
+    // console.log(response.json());
+  });
+}
+// .then(response =>
+//     response.json())
+// .then(({ hits }) => {
+//     this.incrementPage();
+//     console.log(hits);
+//     return hits;
+// });
 
-function renderPosts(arr) { 
-    const markup = arr
+// console.log(renderPosts());
+
+function renderPosts(arr) {
+  // console.log(data);
+  // .map(({ comments, downloads, likes, tags, views, webformatURL }) => {
+
+  const markup = arr
     .map(
-        item => {
-            return`<div class="photo-card">
+      item =>
+        `<div class="photo-card">
         <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
        <div class="info">
         <p class="info-item">
@@ -123,11 +114,49 @@ function renderPosts(arr) {
         </p>
         </div>
           </div>`
-        })
-    .join('');     
-    
-    refs.photosContainer.insertAdjacentHTML("beforeend", markup);     
+    )
+    .join('');
+
+  // photosContainer.insertAdjacentHTML("beforeend", markup);
+
+  refs.photosContainer.innerHTML = markup;
 }
+
+// fetchPostsBtn.addEventListener("click", () => {
+//   // Check the end of the collection to display an alert
+//   if (page > totalPages) {
+//     return toggleAlertPopup();
+//   }
+
+//   fetchPosts()
+//     .then((posts) => {
+//       renderPosts(posts);
+//       // Increase the group number
+//       page += 1;
+
+//       // Replace button text after first request
+//       if (page > 1) {
+//         fetchPostsBtn.textContent = "Fetch more posts";
+//       }
+//     })
+//     .catch((error) => console.log(error));
+// });
+
+// function fetchPosts() {
+//   const params = new URLSearchParams({
+//     _limit: limit,
+//     _page: page
+//   });
+
+//   return fetch(`https://jsonplaceholder.typicode.com/posts?${params}`).then(
+//     (response) => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     }
+//   );
+// }
 
 // function renderPosts(posts) {
 //   const markup = posts
@@ -142,7 +171,6 @@ function renderPosts(arr) {
 //     .join("");
 //   postList.insertAdjacentHTML("beforeend", markup);
 // }
-
 
 // function toggleAlertPopup() {
 //   if (isAlertVisible) {
